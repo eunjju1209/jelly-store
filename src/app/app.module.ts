ec51771c249b90189bd35a1ec4027a9fd4b388d3
eunju <eunjju1209@gmail.com>
2020-06-07 21:56:36 +0900

    typeorm & .env

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+
+// dotenv (환경변수) .env 파일로 빼고 활용하기 위해서
+// https://github.com/nestjsx/nestjs-config
+import { ConfigModule, ConfigService } from 'nestjs-config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as path from 'path';
+
+import { AppController } from '../app.controller';
+import { AppService } from '../app.service';
+
+
+@Module({
+  imports: [
+    ConfigModule.load(path.resolve(__dirname, 'config', '**/!(*.d).{ts,js}')),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './app/app.module';
+
+import * as dotenv from 'dotenv';
+
+dotenv.config({
+  path: '../../.env',
+});
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
